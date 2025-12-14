32121a6a4144cfd9d45bf39c84859824d839467d
24331a05i8 <24331a05i8@mvgrce.edu.in>
2025-12-14 20:21:16 +0530

    Add files via upload

---
+++ d.py
@@ -0,0 +1,6 @@
+import math
+
+print("Value of pi:", math.pi)
+print("Square root of 25:", math.sqrt(25))
+print("Cosine of 0:", math.cos(0))
+print("Sine of 90 degrees:", math.sin(math.radians(90)))
