67509ed3685288998cb6dc6df4610bfd88fb59ec
24331a05i8 <24331a05i8@mvgrce.edu.in>
2025-12-18 20:15:05 +0530

    Add files via upload

---
+++ builtin.py
@@ -0,0 +1,16 @@
+name = input("Enter your name: ")
+print("Hello,", name)
+num = int(input("Enter a number: "))
+print("Absolute value:", abs(num))
+print("Square of number:", pow(num, 2))
+print("Rounded value:", round(3.567, 2))
+numbers = [5, 10, 2, 8]
+print("Sum:", sum(numbers))
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))
+print("Sorted list:", sorted(numbers))
+ch = 'A'
+print("ASCII of", ch, "is:", ord(ch))
+print("Character of 66 is:", chr(66))
+for i in range(1, 4):
+    print("Number:", i)
