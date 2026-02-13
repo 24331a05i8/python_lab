b5c0b79fcb412eb73b6bb6dd276b81bd3d948260
24331a05i8 <24331a05i8@mvgrce.edu.in>
2026-02-13 12:35:52 +0530

    Add files via upload

---
+++ 61.py
@@ -0,0 +1,9 @@
+text = input("Enter a string: ")
+
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Title Case:", text.title())
+print("Length:", len(text))
+print("Count of 'a':", text.count('a'))
+print("Replace 'a' with '@':", text.replace('a', '@'))
+print("Is Alphabetic?:", text.isalpha())
---
+++ 62.py
@@ -0,0 +1,6 @@
+string = input("Enter a string: ")
+letter = input("Enter a letter: ")
+
+count = string.count(letter)
+
+print("The letter occurs", count, "times.")
